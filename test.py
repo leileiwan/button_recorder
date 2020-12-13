10dcbb2dd8684e11ef32a299df9042efd5b60f92
leileiwan <leilei.wan.cn@gmail.com>
2020-12-13 16:10:54 +0800

    test

---
+++ test.py
@@ -0,0 +1,35 @@
+from pywinauto.application import Application
+from PIL import ImageGrab
+import time
+import pyautogui
+
+app = Application(backend='uia').start("button_test_exe.exe")
+window = app["button_test_exe"]
+
+count = 0
+f=open(".\\steps\\1","w")
+while True:
+    try:
+        time.sleep(2)
+        button_name = window.descendants()[0].window_text()
+    except:
+        print("end .....")
+        break
+    else:
+        im = ImageGrab.grab((150, 150, 800, 600))
+        im.save("./images/" + str(count) + ".png")
+        print(button_name+"....")
+        f.write(button_name+"\n")
+        count += 1
+        pyautogui.click(200, 200)
+
+        #DEFSPLITBUTTON can not by click
+        # try:
+        #     window[button_name].click()
+        # except:
+        #     print("ignore click error")
+
+
+f.close()
+
+
